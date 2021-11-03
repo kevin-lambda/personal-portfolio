d5a609c760ef8a4a8790dc201401105cc4d68c22
Kevin Lam <kevin.quoct.lam@gmail.com>
2021-11-03 00:24:20 -0700

    new component1

---
+++ src/components/AllPosts.js
@@ -0,0 +1,9 @@
+import React from "react"
+import { graphql, useStaticQuery } from "gatsby"
+
+//test grabbing all the posts from contentful
+const AllPosts = () => {
+  return <div></div>
+}
+
+export default AllPosts
