370a886c1b225043d15f52c06b946dd0cc04ef99
Kevin Lam <kevin.quoct.lam@gmail.com>
2021-11-02 19:51:34 -0700

    test 1

--- src/pages/devBlogv2.js
+++ src/pages/devBlogv2.js
@@ -1,7 +1,6 @@
 import React from "react"
-
 const devBlogv2 = () => {
-  return <div>test second branch. new changes</div>
+  return <div>test second branch. new changes. additional changes</div>
 }
 
 export default devBlogv2
