acfa319b21531546f4f1ed1bcc817757905712c1
Kevin Lam <kevin.quoct.lam@gmail.com>
2021-11-02 19:52:39 -0700

    commit test 4

--- src/pages/devBlogv2.js
+++ src/pages/devBlogv2.js
@@ -1,6 +1,6 @@
 import React from "react"
 const devBlogv2 = () => {
-  return <div>test second branch. new changes. additional changes</div>
+  return <div>test second branch. new changes. additional changes change 4</div>
 }
 
 export default devBlogv2
